2a81049e5b9f7ab4482999ed1fbd0d4a3617f51e
Norathirah Nordin <norathirahnordin810@gmail.com>
2023-08-24 10:58:21 +0800

    changes on database config

--- config/database.php
+++ config/database.php
@@ -68,17 +68,18 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'dpg-cjiruu7jbvhs73bm0nvg-a.singapore-postgres.render.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'places_api_bo6x'),
+            'username' => env('DB_USERNAME', 'rest_api_user'),
+            'password' => env('DB_PASSWORD', 'AgJId9WVN1rCVgrOmXLPCqNcZkZ6IW0S'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
